f9c35f07a401d250948490d00f86f368c8572319
inmawati <60200112005@uin-alauddin.ac.id>
2015-05-13 21:04:40 +0800

    laporan

---
+++ src/laporan1/pengenalan.java
@@ -0,0 +1,33 @@
+package laporan1;
+
+public class pengenalan {
+
+
+static String nama = "inma"; // variabel global
+		String nim = "60200112005";
+	
+	public static void main(String args[])
+	{
+		System.out.println(nama); //nama di panggil dari variabel global
+		System.out.println("nim"); //nim di panggil dari variabel global
+		System.out.println("Jurusan Teknik Informaatika"); // langsung menampilkan 
+		System.out.println(jumlah(5,2));
+		System.out.println(kurang(1,40));
+		System.out.println(kali(10,20));
+		System.out.println(bagi(234,23));
+		
+	}
+	
+	static int jumlah(int i, int j){ //di buatkan variabel untuk mengeksekusi jumlah
+		return i+j; // pengembalian nilai
+	}
+	static int kurang(int i, int j){
+		return i-j;
+	}
+	static int kali(int i, int j){
+		return i*j;
+	}
+	static float bagi(int i, int j){
+		return i/j;
+	}
+}
---
+++ src/laporan2/NilaiDefault.java
@@ -0,0 +1,27 @@
+package laporan2;
+
+public class NilaiDefault {
+
+	public static void main (String []args){
+		int i=37;
+		int j=12;
+		double x=27.475;
+		double y=7.22;
+		boolean isSuccess = true;
+		long z = 123456;
+		int nilai=0;
+		char answer='a';
+		nilai=(answer=='a')?10:0;
+		System.out.println("nilai = "+nilai+z);
+		System.out.println();
+		System.out.println("modulus operation");
+		System.out.println("i modulus 4="+(i%4));
+		System.out.println("j modulus 4="+(j%4));
+		System.out.println("x modulus 4="+(x%4));
+		System.out.println("y modulus 4="+(y%4));
+		System.out.println("isSuccess = "+isSuccess);
+		
+	}
+
+}
+
